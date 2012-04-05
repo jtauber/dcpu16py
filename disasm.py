ea98d3209d9f05ceddbb43a0613784ea677df600
James Tauber <jtauber@jtauber.com>
2012-04-05 01:30:27 -0400

    greatly simplified disassembler, removing repetition

--- disasm.py
+++ disasm.py
@@ -1,68 +1,52 @@
 #!/usr/bin/env python
 
+INSTRUCTIONS = [None, "SET", "ADD", "SUB", "MUL", "DIV", "MOD", "SHL", "SHR", "AND", "BOR", "XOR", "IFE", "IFN", "IFG", "IFB"]
+IDENTIFERS = ["A", "B", "C", "X", "Y", "Z", "I", "J", "POP", "PEEK", "PUSH", "SP", "PC", "O"]
 
-def disasm(program):
-    offset = 0
+class Disassembler:
     
-    while offset < len(program):
-        w = program[offset]
-        offset += 1
-        
-        operands, opcode = divmod(w, 16)
-        b, a = divmod(operands, 64)
-            
-        if opcode == 0x00:
-            if a == 0x01:
-                print "JSR",
-            else:
-                continue
+    def __init__(self, program):
+        self.program = program
+        self.offset = 0
+    
+    def next_word(self):
+        w = self.program[self.offset]
+        self.offset += 1
+        return w
+    
+    def format_operand(self, operand):
+        if operand < 0x08:
+            return "%s" % IDENTIFERS[operand]
+        elif operand < 0x10:
+            return "[%s]" % IDENTIFERS[operand % 0x08]
+        elif operand < 0x18:
+            return "[0x%02x + %s]" % (self.next_word(), IDENTIFERS[operand % 0x10])
+        elif operand < 0x1E:
+            return "%s" % IDENTIFERS[operand % 0x10]
+        elif operand == 0x1E:
+            return "[0x%02x]" % self.next_word()
+        elif operand == 0x1F:
+            return "0x%02x" % self.next_word()
         else:
-            print [
-                None, "SET", "ADD", "SUB", "MUL", "DIV", "MOD", "SHL",
-                "SHR", "AND", "BOR", "XOR", "IFE", "IFN", "IFG", "IFB"
-            ][opcode],
-                
-            if a < 0x08:
-                print "%s," % ["A", "B", "C", "X", "Y", "Z", "I", "J"][a],
-            elif a < 0x10:
-                print "[%s]," % ["A", "B", "C", "X", "Y", "Z", "I", "J"][a % 0x08],
-            elif a < 0x18:
-                next_word = program[offset]
-                offset += 1
-                print "[0x%02x + %s]," % (next_word, ["A", "B", "C", "X", "Y", "Z", "I", "J"][a % 0x10]),
-            elif a < 0x1E:
-                print "%s," % ["POP", "PEEK", "PUSH", "SP", "PC", "O"][a % 0x18],
-            elif a == 0x1E:
-                next_word = program[offset]
-                offset += 1
-                print "[0x%02x]," % next_word,
-            elif a == 0x1F:
-                next_word = program[offset]
-                offset += 1
-                print "0x%02x," % next_word,
-            else:
-                print "0x%02x," % (a % 0x20),
+            return "0x%02x" % (operand % 0x20)
+    
+    def run(self):
+        while self.offset < len(self.program):
+            w = self.next_word()
             
-        if b < 0x08:
-            print "%s" % ["A", "B", "C", "X", "Y", "Z", "I", "J"][b]
-        elif b < 0x10:
-            print "[%s]" % ["A", "B", "C", "X", "Y", "Z", "I", "J"][b % 0x08]
-        elif b < 0x18:
-            next_word = program[offset]
-            offset += 1
-            print "[0x%02x + %s]" % (next_word, ["A", "B", "C", "X", "Y", "Z", "I", "J"][b % 0x10])
-        elif b < 0x1E:
-            print "%s" % ["POP", "PEEK", "PUSH", "SP", "PC", "O"][b % 0x18]
-        elif b == 0x1E:
-            next_word = program[offset]
-            offset += 1
-            print "[0x%02x]" % next_word
-        elif b == 0x1F:
-            next_word = program[offset]
-            offset += 1
-            print "0x%02x" % next_word
-        else:
-            print "0x%02x" % (b % 0x20)
+            operands, opcode = divmod(w, 16)
+            b, a = divmod(operands, 64)
+            
+            if opcode == 0x00:
+                if a == 0x01:
+                    print "JSR",
+                else:
+                    continue
+            else:
+                print INSTRUCTIONS[opcode],
+                print self.format_operand(a) + ",",
+            
+            print self.format_operand(b)
 
 
 if __name__ == "__main__":
@@ -73,4 +57,5 @@ if __name__ == "__main__":
         0x9037, 0x61c1, 0x7dc1, 0x001a, 0x0000, 0x0000, 0x0000, 0x0000,
     ]
     
-    disasm(example)
+    d = Disassembler(example)
+    d.run()
