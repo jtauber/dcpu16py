fe87f0bc2ed3237c1b44ea244c47f0294395f0f9
James Tauber <jtauber@jtauber.com>
2012-04-07 05:35:48 -0400

    fix import ordering

--- dcpu16.py
+++ dcpu16.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-
+import argparse
 import inspect
 import struct
 import sys
-import argparse
+
 
 try:
     raw_input
 except NameError:
     raw_input = input
+
 
 # offsets into DCPU16.memory corresponding to addressing mode codes
 SP, PC, O = 0x1001B, 0x1001C, 0x1001D
