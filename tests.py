326bd0b6376e536aea4bc314e1104297db32e05f
Anton Barkovsky <swarmer.pm@gmail.com>
2012-04-08 18:31:48 +0300

    Refactor tests

--- tests.py
+++ tests.py
@@ -6,52 +6,41 @@ import subprocess
 ASSEMBLY_OUTPUT = "__test_output.obj"
 SOURCE_DIR = "examples"
 
-def teardown():
+def tearDownModule():
     if os.path.exists(ASSEMBLY_OUTPUT):
         os.remove(ASSEMBLY_OUTPUT)
 
 def example(name):
     return os.path.join(SOURCE_DIR, name + ".asm")
 
-def check_asm(path):
-    code = subprocess.call(["./asm.py", path, ASSEMBLY_OUTPUT])
-    nose.assert_equal(code, 0, "(asm.py)")
-
-def check_pyparsing(path):
-    code = subprocess.call(["./asm_pyparsing.py", path, ASSEMBLY_OUTPUT])
-    nose.assert_equal(code, 0, "(asm_pyparsing.py)")
+def check_path(assembler, path):
+    code = subprocess.call([assembler, path, ASSEMBLY_OUTPUT])
+    nose.assert_equal(code, 0, "({0})".format(assembler))
 
 
 # asm.py
-@nose.with_setup(teardown=teardown)
 def test_example_asm():
-    check_asm("example.asm")
+    check_path("./asm.py", "example.asm")
 
-@nose.with_setup(teardown=teardown)
 def test_hello_asm():
-    check_asm(example("hello"))
+    check_path("./asm.py", example("hello"))
 
-@nose.with_setup(teardown=teardown)
 def test_hello2_asm():
-    check_asm(example("hello2"))
+    check_path("./asm.py", example("hello2"))
 
-@nose.with_setup(teardown=teardown)
 def test_fibonacci_asm():
-    check_asm(example("ique_fibonacci"))
+    check_path("./asm.py", example("ique_fibonacci"))
+
 
 # asm_pyparsing.py
-@nose.with_setup(teardown=teardown)
 def test_example_pyparsing():
-    check_pyparsing("example.asm")
+    check_path("./asm_pyparsing.py", "example.asm")
 
-@nose.with_setup(teardown=teardown)
 def test_hello_pyparsing():
-    check_pyparsing(example("hello"))
+    check_path("./asm_pyparsing.py", example("hello"))
 
-@nose.with_setup(teardown=teardown)
 def test_hello2_pyparsing():
-    check_pyparsing(example("hello2"))
+    check_path("./asm_pyparsing.py", example("hello2"))
 
-@nose.with_setup(teardown=teardown)
 def test_fibonacci_pyparsing():
-    check_pyparsing(example("ique_fibonacci"))
+    check_path("./asm_pyparsing.py", example("ique_fibonacci"))
