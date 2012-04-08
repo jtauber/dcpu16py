af04fe7888f86913fdc4aa7ef9e1f8d971f9cd6a
Anton Barkovsky <swarmer.pm@gmail.com>
2012-04-08 15:55:56 +0300

    More tests

--- tests.py
+++ tests.py
@@ -13,17 +13,45 @@ def teardown():
 def example(name):
     return os.path.join(SOURCE_DIR, name + ".asm")
 
-def check_source(path):
+def check_asm(path):
     code = subprocess.call(["./asm.py", path, ASSEMBLY_OUTPUT])
-    nose.assert_equal(code, 0)
+    nose.assert_equal(code, 0, "(asm.py)")
+
+def check_pyparsing(path):
     code = subprocess.call(["./asm_pyparsing.py", path, ASSEMBLY_OUTPUT])
-    nose.assert_equal(code, 0)
+    nose.assert_equal(code, 0, "(asm_pyparsing.py)")
 
+
+# asm.py
+@nose.with_setup(teardown=teardown)
+def test_example_asm():
+    check_asm("example.asm")
 
 @nose.with_setup(teardown=teardown)
-def test_hello():
-    check_source(example("hello"))
+def test_hello_asm():
+    check_asm(example("hello"))
 
 @nose.with_setup(teardown=teardown)
-def test_example():
-    check_source("example.asm")
+def test_hello2_asm():
+    check_asm(example("hello2"))
+
+@nose.with_setup(teardown=teardown)
+def test_fibonacci_asm():
+    check_asm(example("ique_fibonacci"))
+
+# asm_pyparsing.py
+@nose.with_setup(teardown=teardown)
+def test_example_pyparsing():
+    check_pyparsing("example.asm")
+
+@nose.with_setup(teardown=teardown)
+def test_hello_pyparsing():
+    check_pyparsing(example("hello"))
+
+@nose.with_setup(teardown=teardown)
+def test_hello2_pyparsing():
+    check_pyparsing(example("hello2"))
+
+@nose.with_setup(teardown=teardown)
+def test_fibonacci_pyparsing():
+    check_pyparsing(example("ique_fibonacci"))
